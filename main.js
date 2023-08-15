220fddbeded974fb76067c37591d66d74f818cc9
bhushan354 <deshmukhumesh354@gmail.com>
2023-08-15 09:08:28 +0530

    chanegs

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+// init
 
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+camera.position.z = 1;
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+const material = new THREE.MeshNormalMaterial();
+
+const mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
+
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animation );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// animation
 
-camera.position.z = 5;
+function animation( time ) {
 
-function animate() {
-	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
 	renderer.render( scene, camera );
+
 }
-animate();
